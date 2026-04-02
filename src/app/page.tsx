251c496baaee1603df169354d98ec526a5ae25e3
DevJoySR <joysr@proton.me>
2026-04-02 22:17:54 +0200

    pull

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { VSCodeLayout } from "@/components/vscode";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen">
+      <VSCodeLayout />
+    </main>
+  );
+}
---
+++ src/components/vscode/VSCodeLayout.tsx
@@ -0,0 +1,5 @@
+import VSCodeTopBar from "./VSCodeTopBar";
+import VSCodeActivityBar from "./VSCodeActivityBar";
+import VSCodeSideBar from "./VSCodeSideBar";
+import VSCodeEditor from "./VSCodeEditor";
+import VSCodeStatusBar from "./VSCodeStatusBar";
