be10e53351bdc0b6ff47c53ca4fde57c193498cb
DevJoySR <joysr@proton.me>
2026-04-02 22:54:34 +0200

    fix

--- src/components/vscode/VSCodeLayout.tsx
+++
@@ -1,5 +0,0 @@
-import VSCodeTopBar from "./VSCodeTopBar";
-import VSCodeActivityBar from "./VSCodeActivityBar";
-import VSCodeSideBar from "./VSCodeSideBar";
-import VSCodeEditor from "./VSCodeEditor";
-import VSCodeStatusBar from "./VSCodeStatusBar";
